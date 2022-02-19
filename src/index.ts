402ff2ff0018a57f04a98819f0974b53bb5f859e
Matthias Aarnoutse <mattiejas@outlook.com>
2022-02-19 23:13:47 +0100

    Create and fetch "recipes" from db

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,35 @@
 import Koa from 'koa'
-import Router from '@koa/router'
+import Router from 'koa-router'
+import { PrismaClient } from '@prisma/client'
+import bodyParser from 'koa-bodyparser'
 
 const app = new Koa()
 const router = new Router()
+const prisma = new PrismaClient()
+
+app.use(bodyParser())
 
 router.get('/', async (ctx) => {
   ctx.body = { hello: 'world' }
 })
 
-app.use(router.routes())
+// fetch all recipes
+router.get('/recipes', async (ctx) => {
+  const recipes = await prisma.recipe.findMany()
+  ctx.body = recipes
+})
+
+// create a new recipe
+router.post('/recipes', async (ctx) => {
+  const { title } = ctx.request.body
+  const recipe = await prisma.recipe.create({
+    data: {
+      title,
+    },
+  })
+  ctx.body = recipe
+})
 
 const port = 3000
-app.listen(port)
+app.use(router.routes()).listen(port)
 console.info(`Server running on port ${port}`)
